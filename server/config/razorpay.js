9616d22a168b6b8f201af5d3b76435351e710ac2
aryan <aryansatija2003@gmail.com>
2024-02-02 01:01:48 +0530

    final touch

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,5 +1,5 @@
-const Razorpay = require("razorpay");
-exports.instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY,
-    key_secret: process.env.RAZORPAY_SECRET,
-});
+// const Razorpay = require("razorpay");
+// exports.instance = new Razorpay({
+//     key_id: process.env.RAZORPAY_KEY,
+//     key_secret: process.env.RAZORPAY_SECRET,
+// });
