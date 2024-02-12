da8f7120fafab09c0ce95104c195d40bd83aa0b3
aryan <aryansatija2003@gmail.com>
2024-02-12 12:51:40 +0530

    refactoring

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,15 +1,19 @@
 const nodemailer = require("nodemailer");
-
+require('dotenv').config();
 const mailSender = async (email, title, body) => {
     try{
+            console.log('----------------------------------');
+            console.log(process.env.MAIL_HOST);
+            console.log('----------------------------------');
             let transporter = nodemailer.createTransport({
                 host:process.env.MAIL_HOST,
+                port: 465,
+                secure: true,
                 auth:{
                     user: process.env.MAIL_USER,
                     pass: process.env.MAIL_PASS,
                 }
             })
-
 
             let info = await transporter.sendMail({
                 from: 'CodeBattles',
@@ -17,7 +21,7 @@ const mailSender = async (email, title, body) => {
                 subject: `${title}`,
                 html: `${body}`,
             })
-            console.log(info);
+
             return info;
     }
     catch(error) {
