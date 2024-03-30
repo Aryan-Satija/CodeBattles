51fb30158860baf195617e1d742c4b1134d9e896
aryan <aryansatija2003@gmail.com>
2024-03-30 11:24:41 +0530

    misc

--- server/controllers/Subsection.js
+++ server/controllers/Subsection.js
@@ -1,5 +1,5 @@
 const Section = require("../models/Section");
-const SubSection = require("../models/Subsection");
+const SubSection = require("../models/SubSection");
 const { uploadImageToCloudinary } = require("../utils/imageUploader");
 exports.createSubSection = async (req, res) => {
     try {
